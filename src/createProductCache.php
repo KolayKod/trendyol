ec5ce73c2f58414a9f2188c10eca76d3b79f2b04
Eyüp Klasvento <eyupklasvento@gmail.com>
2023-10-11 17:24:15 +0300

    Update createProductCache.php

--- src/createProductCache.php
+++ src/createProductCache.php
@@ -2,7 +2,22 @@
 class createProductCache extends trendyol {
 
 
+  punlic function saveOnePageData(){
 
+    
+  }
+
+
+  punlic function allCacheMearch(){
+
+    
+  }
+
+
+   punlic function getAllCache(){
+
+    
+  }
 
 
   
