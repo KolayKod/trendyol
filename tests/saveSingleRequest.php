a515e139c7125d07ef871ef544123e24c63efa33
Eyüp Klasvento <eyupklasvento@gmail.com>
2023-10-11 20:04:59 +0300

    Update saveSingleRequest.php

--- tests/saveSingleRequest.php
+++ tests/saveSingleRequest.php
@@ -1,1 +1,22 @@
 <?php
+$fileId = $_GET["fileId"]??exit("FileId belirtilmedi.");
+$page = $_GET["page"]??exit("Page belirtilmedi.");
+$size = $_GET["size"]??exit("Size belirtilmedi.");
+$max = $_GET["max"]??exit("Max belirtilmedi.");
+
+$fileName = $_ENV["mainCacheFolder"]."/tmp/".$sellerId."-".$fileId."-".$_ENV["mainCacheFileName"];
+
+$sayfaIndex = 0;
+$urunIndex = 0;
+
+$categories = array();
+$brands = array();
+
+$allProductsList = array();
+$start = setTimer();
+
+$tryCount = 0;
+$get = tryGetRequest($page,$size);
+
+ $productCache->saveOnePageData(["page"=>$sayfa,"size"=>$size,"approved"=>"true"]); //isteği gelen gelen sonucu kaydediyor. 
+
