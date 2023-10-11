b9956120a54f12df1730c53c32988e5cc19c3026
Eyüp Klasvento <eyupklasvento@gmail.com>
2023-10-11 19:06:08 +0300

    Update createProductCache.php

--- src/createProductCache.php
+++ src/createProductCache.php
@@ -5,33 +5,58 @@ class createProductCache extends trendyol {
    public $cacheSavePath =__DIR__."/cache/trendyol/";
   
 
-  punlic function saveOnePageData(...$arguments){
+  public function saveOnePageData(...$arguments){
                 $productData = $this->getProduct(...$arguments);
         
   }
 
 
-  punlic function allCacheMerge(){
+  public function allCacheMerge(){
 
     
   }
 
 
-   punlic function getAllCache(){
+   public function getAllCache(){
 
     
   }
 
-    punlic function deleteAllTempCacheFiles(){
+    public function deleteAllTempCacheFiles(){
 
     
   }
 
-    punlic function deleteAllLogFiles(){
+    public function deleteAllLogFiles(){
 
     
   }
 
 
+   public function deleteAllLogFiles(){
+
+    
+  }
+
+   public function recursiveRequest(...$arguments){
+       $get = $this->getProduct(...$arguments);
+          if(!isset($get->content)){
+              logWrite("API: ".$page].". sayfa içeriği getirilemedi. ".serialize($get));
+              if($GLOBALS["tryCount"]<=3){
+                  logWrite("Tekrar deneniyor.. Deneme sayısı: ".$GLOBALS["tryCount"]);
+                  $GLOBALS["tryCount"]++;
+                  $get = tryGetRequest();
+              }else{
+                  logWrite($GLOBALS["tryCount"]." deneme başarısız oldu, başaramadık abi.");
+                  exit($GLOBALS["tryCount"]." denemeden sonra içerik hala getirilemedi.");
+              }
+          }
+          return $get;
+  }
+
+
+   
+
+ 
   
 }
