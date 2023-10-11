bf3b835ad0320e6149ea3d4905b5f38b12cf337d
Eyüp Klasvento <eyupklasvento@gmail.com>
2023-10-11 20:13:46 +0300

    Create cacheMerge.php

---
+++ src/cacheMerge.php
@@ -0,0 +1,46 @@
+<?php
+class createProductCache   {
+
+  
+ public function mergeProductsFromJson($data){
+
+   
+ }
+
+public function saveTempData($data){
+
+   
+ }
+
+  public function saveMergedProductList($data){
+
+   
+ }
+
+    public function saveMergedProductList($data){
+
+   
+ }
+
+
+
+  public  function collectData($type, $id, $name, $isSet) {
+    $data = $GLOBALS[$type];
+    if (isset($data[$id])) {
+        $varCount = $data[$id]["variantCount"] ?? 1;
+        $prodCount = $data[$id]["productCount"] ?? 1;
+        $data[$id]["variantCount"] = $varCount + 1;
+        if (!$isSet) {$data[$id]["productCount"] = $prodCount + 1;}
+    } else {
+        $data[$id] = array(
+            ($type === "brands" ? "brandId" : "pimCategoryId") => $id,
+            ($type === "categories" ? "brandName" : "categoryName") => $name,"productCount" => 1,"variantCount" => 1
+        );
+    }
+    $GLOBALS[$type] = $data;
+    return $id;
+}
+
+
+    
+  }
