1de1037c9d0ddafeeb33035f573c5191fdf39930
Eyüp Klasvento <eyupklasvento@gmail.com>
2023-10-11 18:40:08 +0300

    Update createCacheFile.php

--- tests/createCacheFile.php
+++ tests/createCacheFile.php
@@ -1,1 +1,10 @@
+<?php 
 
+include "vendor/autoload.php";
+
+     $apiKey ="sdfdsf";
+     $apiSecret="sdfdsf";
+     $partnerId ="114603";
+    $trendyol =  new trendyol($apiKey,$apiSecret,$partnerId);
+    
+          
