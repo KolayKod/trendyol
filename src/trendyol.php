da06138c442f87d1276c358ee03c0c49887ad20f
Eyüp Klasvento <eyupklasvento@gmail.com>
2025-05-01 17:06:45 +0300

    Update trendyol.php

--- src/trendyol.php
+++ src/trendyol.php
@@ -454,24 +454,7 @@ use trendyolHelpers;
 
       
    
-      public function redirectVariantUrl($contentId,$variantNane,$redirectUrl=false){
-	      
-	      $url = "https://www.trendyol.com/brand/productname-p-".$contentId;
-			        $ch = curl_init();
-				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_HEADER, true);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-				$a = curl_exec($ch);
-				$finalUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-				$v = str_replace(",","-",strtolower($variantNane));
-	      if($redirectUrl ==true){
-		   header("Location:".$finalUrl."?v=".$v);   
-	      }else{
-		      return $finalUrl;
-	      }
-   
-	}
+      
     
 
 	public function sendRequest($url,$method = "GET",$requestData=""){
